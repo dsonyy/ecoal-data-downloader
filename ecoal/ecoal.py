6b8873d679c099f4969b31f134c1fd898f22968f
dsonyy <dsonyyy@gmail.com>
2020-07-08 23:13:13 +0200

    Small improvements

--- ecoal/ecoal.py
+++ ecoal/ecoal.py
@@ -3,16 +3,18 @@ from requests.auth import HTTPBasicAuth
 from io import StringIO
 import time
 import pandas as pd
+import os
 
 IP = "192.168.1.29"
 PASSWORD = "root"
 USER = "root"
+DIR = os.path.normpath("./data/")
 
 def get_day_url(day=None, month=None, year=None, ip=IP):
     if year == None: year = time.localtime().tm_year
     if month == None: month = time.localtime().tm_mon
     if day == None: day = time.localtime().tm_mday
-    return "http://" + str(ip) + "/arch/" + str(year) + "/" + str(month).zfill(2) + "/ARCH" + str(day).zfill(2) + ".CSV"
+    return "http://%s/arch/%s/%02d/ARCH%02d.CSV" % (ip, year, month, day)
 
 def process_day_data(day_data):
     NAMES = {
@@ -25,52 +27,23 @@ def process_day_data(day_data):
     "t1_value":	        "Temperatura za zawrotem",
     "tsp_value":        "Temperatura spalin",
     "tzew_act":         "Temperatura zewnętrzna",
-    
-    "t2_value":	        "",
-
-    "kot_tact":	        "Temperatura zadana",
-    "ob1_pok_tact":	    "Temperatura wewn. CO1",
-    "ob1_pok_tzad":     "Temperatura zadana wewn. CO1",
-    "ob1_zaw4d_tzad":	"Temperatura zadana za zaworem CO1",
-    "ob1_zaw4d_pos":    "",
-    "ob2_pok_tact":	    "",
-    "ob2_pok_tzad":     "",
-    "cwu_tact":	        "Temperatura zadana CWU",
-    "ob3_pok_tact":	    "",
-    "ob3_pok_tzad":	    "",
-    "ob3_zaw4d_tzad":   "",
-    "ob3_zaw4d_pos":	"",
-    "ob3_t1":	        "",
-    "ob3_t2":	        "",
-    "ob4_pok_tact":	    "",
-    "ob4_pok_tzad":     "",
-    "ob4_zaw4d_tzad":	"",
-    "ob4_zaw4d_pos":	"",
-    "ob4_t1":	        "",
-    "ob4_t2":           "",
-    "ob5_pok_tact":     "",
-    "ob5_pok_tzad":	    "",
-    "ob5_zaw4d_tzad":	"",
-    "ob5_zaw4d_pos":	"",
-    "ob5_t1":	        "",
-    "ob5_t2":           "",
-    "ob6_pok_tact":     "",
-    "ob6_pok_tzad":     "",
-    "ob6_zaw4d_tzad":   "",
-    "ob6_zaw4d_pos":    "",
-    "ob6_t1":	        "",
-    "ob6_t2":           "",
     }
 
     day_data = day_data.decode()
     day_data = StringIO(day_data)
     day_data = pd.read_csv(day_data, ";")
-    # day_data.datetime = pd.to_datetime(day_data.datetime + TIMEZONE_OFFSET, unit="s") # convert dates
     day_data = day_data.iloc[:, :-1] # remove last unnamed column
-    day_data.rename(columns={
-        }, inplace=True)
+    day_data.rename(columns=NAMES, inplace=True)
         
     return day_data
+
+def save_day_data(day_data, dir=None, last_timestamp=None):
+    files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+    files.sort()
+    latest = pd.read_csv(os.path.join(dir, files[-1]), sep=";")
+    timestamp = latest.iloc[-1, 0]
+    # day_data.loc[(day_data.datetime >= timestamp)]
+    
 
 def download_day_data(day=None, month=None, year=None, user=USER, password=PASSWORD, ip=IP, dir=None):
     response = requests.get(
