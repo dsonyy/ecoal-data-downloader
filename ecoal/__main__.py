afb3e834a2e8adb20bf20db03e22940ccd2f97b0
dsonyy <dsonyyy@gmail.com>
2020-07-09 16:43:17 +0200

    Data watcher works

--- ecoal/__main__.py
+++ ecoal/__main__.py
@@ -1,11 +1,31 @@
-from ecoal import get_last_saved_timestamp
-from ecoal import save_day
+from ecoal import get_day_url
+from ecoal import get_day_path
+from ecoal import get_last_day_file
+from ecoal import get_last_day_file_timestamp
 from ecoal import download_day
-from ecoal import download_data
+from ecoal import download_day_file
+from ecoal import update_dir
+from time import sleep
+from io import StringIO
+from sys import stderr
 
 def main():
-    print(get_last_saved_timestamp())
-    download_data()
+    update_dir()
+    df = download_day()
+    if df is not None:
+        timestamp = df["timestamp"].iloc[-1]
+    else:
+        print("No data from today.", file=stderr)
+        return
+    while True:
+        sleep(30)
+        df = download_day()
+        df = df.loc[(df["timestamp"] > timestamp)]
+        if len(df) > 0:
+            s = StringIO()
+            df.to_csv(s, index=False, sep=",", header=False)
+            print(s.getvalue(), end="")
+            df.to_csv(get_day_path(), mode='a', header=False, index=False)
 
 if __name__ == "__main__":
     main()
--- ecoal/ecoal.py
+++ ecoal/ecoal.py
@@ -5,6 +5,7 @@ import time
 import pandas as pd
 import os
 from datetime import datetime, timedelta
+import sys
 
 IP = "192.168.1.29"
 PASSWORD = "root"
@@ -12,40 +13,18 @@ USER = "root"
 DIR = os.path.normpath("./data/")
 
 def get_day_url(day=None, month=None, year=None, ip=IP):
-    if year == None: year = datetime.now().year
-    if month == None: month = datetime.now().month
-    if day == None: day = datetime.now().day
+    if year == None: year = datetime.utcnow().year
+    if month == None: month = datetime.utcnow().month
+    if day == None: day = datetime.utcnow().day
     return "http://%s/arch/%s/%02d/ARCH%02d.CSV" % (ip, year, month, day)
 
 def get_day_path(day=None, month=None, year=None, dir=DIR):
-    if year == None: year = datetime.now().year
-    if month == None: month = datetime.now().month
-    if day == None: day = datetime.now().day
+    if year == None: year = datetime.utcnow().year
+    if month == None: month = datetime.utcnow().month
+    if day == None: day = datetime.utcnow().day
     return os.path.join(dir, "%d-%02d-%02d.csv" % (year, month, day))
 
-
-def process_day_data(day_data):
-    NAMES = {
-    "tkot_value":	    "Temperatura kotła",
-    "tpow_value":	    "Temperatura powrotu",
-    "tpod_value":	    "Temperatura podajnika",    
-    "tcwu_value":       "Temperatura CWU",
-    "twew_value":	    "Temperatura wewn. CO1",
-    "tzew_value":	    "Temperatura zewnętrzna",
-    "t1_value":	        "Temperatura za zawrotem",
-    "tsp_value":        "Temperatura spalin",
-    "tzew_act":         "Temperatura zewnętrzna",
-    }
-
-    day_data = day_data.decode()
-    day_data = StringIO(day_data)
-    day_data = pd.read_csv(day_data, ";")
-    day_data = day_data.iloc[:, :-1] # remove last unnamed column
-    day_data.rename(columns=NAMES, inplace=True)
-        
-    return day_data
-
-def get_last_saved_day(dir=DIR):
+def get_last_day_file(dir=DIR):
     files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
     files.sort()
     if not files:
@@ -53,42 +32,63 @@ def get_last_saved_day(dir=DIR):
     latest = pd.read_csv(os.path.join(dir, files[-1]), sep=",")
     return latest
 
-def get_last_saved_timestamp(dir=DIR):
-    last_day = get_last_saved_day(dir)
+def get_last_day_file_timestamp(dir=DIR):
+    last_day = get_last_day_file(dir)
     if last_day is None:
         return None
-    return last_day.iloc[-1, 0]
+    return last_day.iloc[-1, 0]  
 
-def save_day(day_data, dir=None, last_timestamp=None):
-    files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
-    files.sort()
-    latest = pd.read_csv(os.path.join(dir, files[-1]), sep=";")
-    timestamp = latest.iloc[-1, 0]
-    # day_data.loc[(day_data.datetime >= timestamp)]    
+def process_day_raw(day_raw):
+    NAMES = {
+        "datetime":     "timestamp",
+        "tkot_value":	"Temperatura kotła",
+        "tpow_value":	"Temperatura powrotu",
+        "tpod_value":	"Temperatura podajnika",    
+        "tcwu_value":   "Temperatura CWU",
+        "twew_value":	"Temperatura wewn. CO1",
+        "tzew_value":	"Temperatura zewnętrzna",
+        "t1_value":	    "Temperatura za zawrotem",
+        "tsp_value":    "Temperatura spalin",
+        "tzew_act":     "Temperatura zewnętrzna",
+    }
+    day_raw = day_raw.decode()
+    day_raw = StringIO(day_raw)
+    day_raw = pd.read_csv(day_raw, ";")
+    day_raw = day_raw.iloc[:, :-1] # remove last unnamed column
+    day_raw.rename(columns=NAMES, inplace=True)
+    return day_raw
 
-def download_day(day=None, month=None, year=None, user=USER, password=PASSWORD, ip=IP, dir=None):
+def download_day(day=None, month=None, year=None, user=USER, password=PASSWORD, ip=IP, dir=DIR):
+    url = get_day_url(day, month, year, ip)
     response = requests.get(
-        get_day_url(day, month, year, ip), 
+        url, 
         auth=HTTPBasicAuth(user, password), 
         timeout=1)
     if response.status_code == 404:
+        print("404 Server response error: ", url, file=sys.stderr)
         return None
     response.raise_for_status()
-    day_data = process_day_data(response.content)
-    if dir:
-        if year == None: year = datetime.now().year
-        if month == None: month = datetime.now().month
-        if day == None: day = datetime.now().day
-        try:
-            day_data.to_csv(get_day_path(day, month, year), sep=",", index=False)
-        except Exception as e:
-            print(e)
-    return day_data
+    return process_day_raw(response.content)
+
+def download_day_file(day=None, month=None, year=None, user=USER, password=PASSWORD, ip=IP, dir=DIR):
+    day_data = download_day(day, month, year, user, password, ip, dir)
+    try:
+        day_data.to_csv(get_day_path(day, month, year), sep=",", index=False)
+        return day_data
+    except Exception as e:
+        print("Unable to save data to file: ", e, file=sys.stderr)
         
-def download_data(since_timestamp=None, user=USER, password=PASSWORD, dir=DIR, ip=IP):
-    if since_timestamp == None: since_timestamp = get_last_saved_timestamp(dir)
-    day = datetime.fromtimestamp(since_timestamp)
-    today = datetime.now() + timedelta(days=1)
-    while day <= today:
-        download_day(day.day, day.month, day.year, user=user, password=password, dir=dir, ip=ip)
-        day += timedelta(days=1)
+def update_dir(user=USER, password=PASSWORD, ip=IP, dir=DIR):
+    since_timestamp = get_last_day_file_timestamp(dir)
+    if since_timestamp is None:
+        download_day_file(user=user, password=password, ip=ip, dir=dir)
+        return
+    day = datetime.utcnow()
+    day_data = download_day_file(day.day, day.month, day.year, user, password, ip, dir)
+    timestamp = day_data.iloc[0, 0]
+    while timestamp >= since_timestamp:
+        day -= timedelta(days=1)
+        day_data = download_day_file(day.day, day.month, day.year, user, password, ip, dir)
+        if day_data is None:
+            continue
+        timestamp = day_data.iloc[0, 0]
